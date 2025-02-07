1b3346eaa25625592082d4e0f7289a7974a71cbb
massmastrangelo <6235094@edu.vaniercollege.qc.ca>
2025-02-07 11:44:43 -0500

    exercise

---
+++ public_html/web2/2d_transform.js
@@ -0,0 +1,11 @@
+
+
+console.log('2d_transform.js loaded');
+
+
+let car = document.querySelector("#messi");
+
+car.addEventListener('click', function (event) {
+  
+  this.classList.toggle('move-right');
+})
